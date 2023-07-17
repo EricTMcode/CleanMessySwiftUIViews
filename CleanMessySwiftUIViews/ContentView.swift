33a2ea9e76362d7c954e26cf72cd642e5ae1a693
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-17 13:44:58 +0200

    -Add Image assets

--- CleanMessySwiftUIViews/ContentView.swift
+++ CleanMessySwiftUIViews/ContentView.swift
@@ -17,25 +17,51 @@ import SwiftUI
 struct User: Identifiable {
     var id = UUID()
     var name = ""
-    static let example: [User] = [ User(name: "John"), User(name: "Chloe")]
+    static let examples: [User] = [ User(name: "John"), User(name: "Chloe")]
 }
 
 
 struct ContentView: View {
-    @State private var users: [User] = [User.example.randomElement()!]
+    @State private var users: [User] = [User.examples.randomElement()!]
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(users) { user in
                     HStack {
+                        Image(user.name)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .overlay {
+                                Circle()
+                                    .stroke(lineWidth: 2)
+                                    .foregroundColor(.secondary)
+                            }
+                            .shadow(radius: 4, x:4, y:4)
+                        
                         Text("\(user.name)")
                     }
                 }
             }
+            .navigationTitle("Users")
+            .toolbar {
+                addButton
+            }
+        }
+    }
+    
+    var addButton: some View {
+        Button {
+            users.append(User.examples.randomElement()!)
+        } label: {
+            Image(systemName: "plus")
+                .font(.title2)
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
