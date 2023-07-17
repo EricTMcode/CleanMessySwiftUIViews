ea7d70bf130cb12ba9b6f3b61d0b563106cd7404
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-17 13:25:24 +0200

    Initial Commit

---
+++ CleanMessySwiftUIViews/CleanMessySwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMessySwiftUIViewsApp.swift
+//  CleanMessySwiftUIViews
+//
+//  Created by Eric on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanMessySwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
